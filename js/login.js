ee9ee9bfb735d668f12815693d9800975c83002c
Joaquin de los Santos <joaquindelossantos18@outlook.com>
2022-08-21 19:39:26 -0300

    Login y funcionalidad/validacion

---
+++ js/login.js
@@ -0,0 +1,22 @@
+function dataValidation(){
+  const email = document.getElementById('email');
+  const password = document.getElementById('password');
+  const loginBtn = document.getElementById('loginBtn');
+  const errorMsg = document.getElementById('errorMsg');
+
+  loginBtn.addEventListener('click', () => {
+
+    if(email.value == '' || password.value == ''){
+      //alert('Porfavor llene ambos campos');
+      errorMsg.style.color = 'red';
+      errorMsg.style.paddingBottom = '10px';
+      errorMsg.style.display = 'inline-block';
+
+    }else{
+      window.location.href = "home.html";
+    }
+  })
+}
+
+
+dataValidation();
